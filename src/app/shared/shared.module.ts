762f7e76af85341c83cce86e00eaf32c87c30a8f
cynthia <cynthiabeltran11@gmail.com>
2020-03-30 14:10:49 -0600

    Agregamos y modificamos modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
+import { HeaderComponent } from './header/header.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    BreadcrumsComponent,
+    HeaderComponent,
+    NopagefoundComponent,
+    SidebarComponent,
+  ],
+  exports: [
+    BreadcrumsComponent,
+    HeaderComponent,
+    NopagefoundComponent,
+    SidebarComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
